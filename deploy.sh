bdc59cd00b9c8a3f69110e8cdd22ffe8d10f67a9
Fayez Eyhab Rajab <fayezrajab92@gmail.com>
2026-06-05 16:04:47 +0300

    update script

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ GITHUB_REPO="https://github.com/fayezrajab2004/Laravel-Cloud-Assignment.git"
 PROJECT_PATH="/home/deployer/Laravel-Cloud-Assignment"
 DATE=$(date +%Y%m%d%H%M%S)
 
-# الربط مع السيرفر من خلال SSH (ملاحظة: السطر متصل مع EOF)
+# الربط مع السيرفر من خلال SSH
 ssh -i server_key.pem -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP << EOF
 
     echo "Connected to the server successfully."
@@ -28,20 +28,29 @@ ssh -i server_key.pem -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP << EOF
     mkdir -p $PROJECT_PATH/releases/$DATE
     git clone $GITHUB_REPO $PROJECT_PATH/releases/$DATE
 
-    cd $PROJECT_PATH/releases/$DATE
-
-    # n. نظام releases لضمان Zero-Downtime (ربط الملفات المشتركة)
+    #نظام releases لضمان Zero-Downtime (ربط الملفات المشتركة)
     ln -s $PROJECT_PATH/shared/.env $PROJECT_PATH/releases/$DATE/.env
     ln -s $PROJECT_PATH/shared/storage $PROJECT_PATH/releases/$DATE/storage
+
+    # تكملة Zero-Downtime
+    echo "Switching to the new release..."
+    ln -sfn $PROJECT_PATH/releases/$DATE $PROJECT_PATH/current
+
+    # الدخول للمجلد الحالي لضمان تحديث الحاويات الموجودة
+    cd $PROJECT_PATH/current
+
+    # إعطاء الصلاحيات للمجلدات
+    echo "Setting permissions..."
+    sudo chmod -R 777 storage
+    sudo chmod -R 777 bootstrap/cache
+    sudo chmod -R 777 database
 
     # إنشاء كونتينر جديد وتنزيل البيئة (Apache, MySQL, PHP) وإعداد Queue
     echo "Building and running Docker containers..."
     docker compose up -d --build
 
-    # إعطاء الصلاحيات للمجلدات وتثبيت Sentry و Horizon
-    echo "Setting permissions and installing packages..."
-    sudo chmod -R 777 storage
-    sudo chmod -R 777 bootstrap/cache
+    # تثبيت Sentry و Horizon
+    echo "Installing packages..."
     docker compose exec -T app composer require sentry/sentry-laravel
     docker compose exec -T app composer require laravel/horizon
     docker compose exec -T app php artisan horizon:install
@@ -52,14 +61,13 @@ ssh -i server_key.pem -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP << EOF
     docker compose exec -T app php artisan optimize:clear
     docker compose exec -T app php artisan migrate --force
 
-    # تكملة Zero-Downtime (تغيير المؤشر للإصدار الجديد)
-    echo "Switching to the new release..."
-    ln -sfn $PROJECT_PATH/releases/$DATE $PROJECT_PATH/current
+
 
     # إعداد linux cron job خاصة بـ backup للداتابيز في مجلد مستقل
     echo "Setting up cron job for database backup..."
-    echo "0 0 * * * docker exec db_container mysqldump -u root -p password database_name > $PROJECT_PATH/backups/db_backup.sql" > cron_backup.txt
+    echo "0 0 * * * cp $PROJECT_PATH/current/database/database.sqlite $PROJECT_PATH/backups/db_backup.sqlite" > cron_backup.txt
     crontab cron_backup.txt
+    rm cron_backup.txt
 
     echo "Deployment finished completely!"
 
